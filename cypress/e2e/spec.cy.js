7160e6bbcd0d4616944af4f9bc1e462ceb427126
Walmyr <wlsf82@gmail.com>
2023-02-13 02:10:30 +0100

    Leave just the download button
    So I can record a video to demonstrate how to test especifically
    that.

--- cypress/e2e/spec.cy.js
+++ cypress/e2e/spec.cy.js
@@ -1,14 +1,10 @@
-describe('cy.tat("anchors")', { viewportWidth: 380, viewportHeight: 200}, () => {
+const options = { viewportWidth: 380, viewportHeight: 200}
+
+describe('cy.readFile()', options, () => {
   beforeEach(() => cy.visit('./src/index.html'))
 
-  it('"POST me" anchor triggers a POST request on click', () => {
-    cy.intercept('POST', 'https://api.example.com', { body: 'Yo!' }).as('postReq')
-    cy.contains('a', 'POST me').click().blur()
-    cy.wait('@postReq').its('response.body').should('be.equal', 'Yo!')
-  })
-
   it('"Download" anchor downloads a file on click', () => {
-    cy.contains('a', 'Download').click().blur()
+    cy.contains('a', 'Download').click()
     cy.readFile('./cypress/downloads/helloworld.txt').as('fileContent')
     cy.get('@fileContent').should('be.equal', 'Hello, World!')
   })
