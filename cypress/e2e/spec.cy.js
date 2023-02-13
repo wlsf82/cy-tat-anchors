18ac69fb68f14f2272b2bf9c0fc7bd261855459a
Walmyr <wlsf82@gmail.com>
2023-02-13 02:11:41 +0100

    Leave just the post me button
    So I can record a video to demonstrate how to test especifically
    that.

--- cypress/e2e/spec.cy.js
+++ cypress/e2e/spec.cy.js
@@ -1,15 +1,11 @@
-describe('cy.tat("anchors")', { viewportWidth: 380, viewportHeight: 200}, () => {
+const options = { viewportWidth: 380, viewportHeight: 200}
+
+describe('cy.intercept()', options, () => {
   beforeEach(() => cy.visit('./src/index.html'))
 
   it('"POST me" anchor triggers a POST request on click', () => {
     cy.intercept('POST', 'https://api.example.com', { body: 'Yo!' }).as('postReq')
-    cy.contains('a', 'POST me').click().blur()
+    cy.contains('a', 'POST me').click()
     cy.wait('@postReq').its('response.body').should('be.equal', 'Yo!')
-  })
-
-  it('"Download" anchor downloads a file on click', () => {
-    cy.contains('a', 'Download').click().blur()
-    cy.readFile('./cypress/downloads/helloworld.txt').as('fileContent')
-    cy.get('@fileContent').should('be.equal', 'Hello, World!')
   })
 })
